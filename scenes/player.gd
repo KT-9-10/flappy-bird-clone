cb9ed8fa33b12411be3c47938373555a0b37c6e4
KT-9-10 <60833391+KT-9-10@users.noreply.github.com>
2026-03-20 10:24:13 +0900

    Add player rotation

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var jump_strength := 280
+@export var jump_strength := 300
 @export var gravity := 800
 @export var max_fall_speed := 500
 
@@ -8,9 +8,13 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	# ジャンプ入力受付
 	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_strength
+		velocity.y = 0
+		velocity.y -= jump_strength
 	# 重力の適用
 	velocity.y += gravity * delta
+	# Spriteの回転
+	#rotation = deg_to_rad(velocity.y * 0.1)
+	rotation = deg_to_rad(clamp(velocity.y * 0.1, -15, 45))
 	# 最大落下スピードを超えないようにする
 	velocity.y = min(velocity.y, max_fall_speed)
 	
